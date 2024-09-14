4b15e37fa843d1a6bc6d207af37d57effea2eacc
Peter Szabados <kangirigungi@gmail.com>
2024-09-14 19:05:44 +0200

    Disable swapfile

--- init.lua
+++ init.lua
@@ -46,9 +46,6 @@ vim.opt.smartcase = true
 -- Keep signcolumn on by default
 vim.opt.signcolumn = 'auto'
 
--- Decrease update time
-vim.opt.updatetime = 500
-
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
 vim.opt.timeoutlen = 600
@@ -78,6 +75,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.linebreak = false
 vim.opt.autoread = true
+vim.opt.swapfile = false
+vim.opt.updatetime = 500
 
 require 'custom.keymaps'
 require 'custom.commands'
@@ -108,17 +107,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   require 'custom.plugins.which-key', -- Useful plugin to show you pending keybinds.
   require 'custom.plugins.telescope',
@@ -147,28 +135,7 @@ require('lazy').setup({
   { 'bkad/CamelCaseMotion' },
   { 'ngg/vim-gn' },
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  --require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
